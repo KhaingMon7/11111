4ce47cea0e721cf6903f8ee47eed6692112a765d
KhaingMon7 <zawtunwai.cloudflare@gmail.com>
2026-05-19 21:41:11 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -232,7 +232,7 @@ if jq . >/dev/null 2>&1 <<<'{}'; then
     .listen = (."listen" // ":5667") |
     .cert = "/etc/zivpn/zivpn.crt" |
     .key  = "/etc/zivpn/zivpn.key" |
-    .obfs = (."obfs" // "zivpn") |
+    .obfs = (."tls" // "zivpn") |
     .server = $ip
   ' "$CFG" > "$TMP" && mv "$TMP" "$CFG"
 fi
